c95fb8de8f7db9cf66f6f6865f87cc683d8aca33
srinu2003 <tsrin2003@gmail.com>
2024-03-10 01:23:36 +0530

    Update GUI layout and add new frames and buttons

--- GUI.py
+++ GUI.py
@@ -34,27 +34,54 @@ title_label.pack()
 
 # top entry field
 top_text_field = tk.Text(app, width=50, height=10, background='light blue')  # widget
-# top_text_field.pack(fill='x',padx=5,pady=2)
+
 top_text_field.pack(side='top', expand=True, fill='both', padx=10, pady=10)
 
 # input Frame 
 key_frame = ttk.Frame(app)
-
 key_lable = ttk.Label(key_frame, text="Enter your KEY:")
 key_lable.pack(side='left', pady=0)
 
 entry = ttk.Entry(key_frame, show=u"\u25CF", width=32)  # ,textvariable='hellll')
 entry.pack(side='left', padx=10)
 
-key_frame.pack(pady=10, side='top')
+key_frame.pack(pady=10, side='top',fill='x')
 
-# Button field
-button_frame = ttk.Frame(app)
+# Radio button field
+radio_frame = ttk.Frame(app, relief = tk.GROOVE, padding = 10)
+radio_bool = tk.BooleanVar(value=True)
+file_frame = ttk.Frame(app, relief = tk.GROOVE, padding = 10)
 
-ttk.Button(button_frame, text='Encrypt', command=lambda: print('Encrypt Presed')).pack(side='left', padx=10)
-ttk.Button(button_frame, text='Decrypt', command=browse_files).pack(side='left', padx=10)
+debug_mode = True
 
-button_frame.pack()
+def radio_func():
+    print(radio_bool.get())
+    if debug_mode:
+        if radio_bool.get():
+            print('Encrypt Selected')
+        else:
+            print('Decrypt Selected')
+
+# widgets
+encrypt_radio = ttk.Radiobutton( radio_frame, 
+    text = 'Encrypt', 
+    value = True, 
+    command = radio_func, 
+    variable = radio_bool).pack(side='left',fill='y', padx = 20)
+
+decrypt_radio = ttk.Radiobutton( radio_frame, 
+	text = 'Decrypt', 
+	value = False, 
+	command = radio_func, 
+	variable = radio_bool).pack(side='left',fill='y', padx= 20)
+
+# radio_frame.pack(fill='x', pady=10, side='top')
+radio_frame.pack(side='left')
+
+open_file_button = ttk.Button(file_frame, text='Open File', command=browse_files).pack(side="top", padx=10, pady = 10)
+save_file_button = ttk.Button(file_frame, text='Save File', command=browse_files).pack(side="top", padx=10, pady = 10)
+file_frame.pack(fill='x', pady=10, side='right')
+
 # Output field
 output_lable = ttk.Label(app, text='output', font='calibri 24', textvariable='str')
 
